d639be61866e45f56ba8352f0b226c6f2db380d2
Jonathan Ody <jonathanody@users.noreply.github.com>
2015-04-18 21:18:50 +0100

    Added grunt.js for automating build tasks

---
+++ gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function (grunt) {
+    'use strict';
+    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            options: { force: true },
+            release: ['./../../../Release/SharpenTheDevSaw/Deploy/*'],
+            buildFiles: ['./../../../Release/SharpenTheDevSaw/Build/*']
+        },
+        concat: {
+			//options: { separator: ';' },
+			release: {
+				src: ['Scripts/*.js'],
+				dest: './../../../Release/SharpenTheDevSaw/Build/sharpenTheDevSaw.js'
+			}
+		},
+		uglify: {
+			release: {
+				files: {
+					'./../../../Release/SharpenTheDevSaw/Deploy/Scripts/sharpenTheDevSaw.min.js' : ['Scripts/settings.js', 'Scripts/statsCalculator.js', 'Scripts/AppViewModel.js'],
+					'./../../../Release/SharpenTheDevSaw/Deploy/Scripts/Libs/libs.min.js' : ['Scripts/Libs/knockout-3.3.0.js', 'Scripts/Libs/countUp.min.js', 'Scripts/Libs/classie.js']
+				}
+			},
+			options: {
+				compress: {
+					drop_console: true
+				}
+			}
+		},
+		cssmin: {
+			release: {
+				files : {
+					'./../../../Release/SharpenTheDevSaw/Deploy/Content/style.min.css': ['Content/bootstrap.css', 'Content/animate.css', 'Content/sharpenTheSaw.css']
+				}
+			}
+		},
+		processhtml: {
+			options: {
+				data: {
+					styles:
+					    '<link href="Content/style.min.css" rel="stylesheet"/>',
+					scripts:
+					    '<script src="Scripts/Libs/libs.min.js"></script>' +
+					    '<script src="Scripts/sharpenTheDevSaw.min.js"></script>'
+				}
+			},
+			release: {
+				files: {
+					'./../../../Release/SharpenTheDevSaw/Build/index.html': ['index.html']
+				}
+			}
+		},
+		htmlmin: {
+			release: {
+				options: {
+					removeComments: true,
+					removeOptionalTags: true,
+					collapseWhitespace: true
+				},
+				files: {
+					'./../../../Release/SharpenTheDevSaw/Deploy/index.html': ['./../../../Release/SharpenTheDevSaw/Build/index.html']
+				}
+			}
+		}
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+    grunt.registerTask('default', ['clean']);
+    grunt.registerTask('build', ['clean', 'uglify', 'cssmin', 'processhtml', 'htmlmin']);
+};
